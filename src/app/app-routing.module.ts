dffbcf0baf3e67528622e96459dab48e3714b94d
TakahiroSono <taka.sono889@gmail.com>
2021-05-05 22:27:06 +0900

    add all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LinksComponent } from './pages/links/links.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'favorite', component: FavoriteComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'links', component: LinksComponent},
   {path: '**', component: PageNotFoundComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { NavigationComponent } from './common/navigation/navigation.component';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { LinksComponent } from './pages/links/links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PageNotFoundComponent,
-    NavigationComponent
+    NavigationComponent,
+    FavoriteComponent,
+    SkillsComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
